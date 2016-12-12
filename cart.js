77ba97f6908fc4dc930739be24e5e24fd2a943de
Jamesmalo <9jmalo@stpatrick.org>
2016-12-12 10:53:15 -0600

    day 1 work

---
+++ cart.js
@@ -0,0 +1,14 @@
+class Cart{
+  //What is the first part of every class? Type it below.
+  constructor(i,q){
+    this.itemList = i;
+    this.itemQuantity = q;
+  }
+
+
+  //Type the instance functions below this comment.
+  additem(i,q){
+    
+  }
+
+}
---
+++ item.js
@@ -0,0 +1,15 @@
+class Item{
+  //Fill in the item class below this comment.
+  constructor(n,p,s){
+    this.name = n;
+    this.price = p;
+    this.shipping = s;
+  }
+}
+
+//Create your three test items below this comment.
+var videogame1 = {"Call of Duty:Infinte Warfare","$59.99","$5.00"}
+
+var videogame2 = {"Madden NFL 17","$59.99","$5.00"}
+
+var videogame3 = {"Battlefield 1","$59.99","$5.00"}
