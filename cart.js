805dfbd330e164b3a5bc0fc714fe49c727b6c690
Jamesmalo <9jmalo@stpatrick.org>
2016-12-14 11:25:46 -0600

    day 3 work

--- cart.js
+++ cart.js
@@ -8,12 +8,21 @@ class Cart{
 
   //Type the instance functions below this comment.
   additem(i,q){
-
+    this.itemList.push(i);
+    this.itemQuantity.push(q);
   }
   totalcart(){
+    let total = 0;
 
+    for(let i=0;i<this.itemQuantity.length;i++){
+      total += this.itemList[i].price*this.itemQuantity[i].price;
+      return total;
+    }
   }
   subcart(days){
-    var shipdays3 = 
+    let subCart = new Cart([],[]);
+    for(let i=0;i < this.itemList.length;i++){
+      this.
+    }
   }
 }
--- password.js
+++ password.js
@@ -1,11 +1,34 @@
 class Password{
   //The first part of the class.
-
+  constructor(pu,pr){
+    this.publickey = pu;
+    this.privatekey = pr;
+  }
 
 
   //Instance functions below this comment.
+  validPublicKey(){
+    for(let i=0;i<this.publickey.length;i++){
+      if(this.publickey.length<8){
+        return false;
+      }
+      else if(this.publickey.length>25;){
+        return false;
+      }
+      else if(this.publickey.length>=8){
+        return true;
+      }
+      else if(this.publickey.length<=25){
+        return true
+      }
+      }
+    }
+  }
+  validPrivateKey(){
 
-
-
+  }
   //Static function below this comment.
+  static.makePrivateKey(){
+
+  }
 }
